2cc9d537fcdb1d33d90bf2da778645bb2123fcb8
Petr Vnenk <petr.vnenk@gmail.com>
2020-08-11 16:43:44 +0200

    Remove comments

--- app/src/components/ProjectDiagram/ProjectDiagram.tsx
+++ app/src/components/ProjectDiagram/ProjectDiagram.tsx
@@ -5,28 +5,19 @@ import * as go from "gojs";
 import { ReactDiagram } from "gojs-react";
 import { GetProject } from "../ProjectView/__generated__/GetProject";
 
-// ...
-
-/**
- * Diagram initialization method, which is passed to the ReactDiagram component.
- * This method is responsible for making the diagram and initializing the model and any templates.
- * The model's data should not be set here, as the ReactDiagram component handles that via the other props.
- */
 function initDiagram() {
   const $ = go.GraphObject.make;
-  // set your license key here before creating the diagram: go.Diagram.licenseKey = "...";
   const diagram = $(go.Diagram, {
-    "undoManager.isEnabled": true, // must be set to allow for model change listening
-    // 'undoManager.maxHistoryLength': 0,  // uncomment disable undo/redo functionality
+    "undoManager.isEnabled": true,
     autoScale: go.Diagram.Uniform,
     layout: $(go.ForceDirectedLayout, {
-      defaultSpringLength: 40, // forces nodes closer together (default: 50)
-      defaultSpringStiffness: 0.05, // forces nodes closer together (default: 0.05)
+      defaultSpringLength: 40,
+      defaultSpringStiffness: 0.05,
       isOngoing: false,
       isRealtime: false,
     }),
     model: $(go.GraphLinksModel, {
-      linkKeyProperty: "key", // IMPORTANT! must be defined for merges and data sync when using GraphLinksModel
+      linkKeyProperty: "key",
       nodeKeyProperty: "key",
     }),
   });
@@ -99,23 +90,22 @@ function handleModelChange(changes: any) {
 
 // render function...
 export function ProjectDiagram(props: GetProject) {
+  console.log(props.project.nodes);
   return (
-    <div>
-      <ReactDiagram
-        skipsDiagramUpdate={false}
-        initDiagram={initDiagram}
-        divClassName="diagram-component"
-        nodeDataArray={props.project.nodes.map((node) => ({
-          key: node.uniqueName,
-          text: node.uniqueName,
-        }))}
-        linkDataArray={props.project.edges.map((edge) => ({
-          key: edge.id,
-          from: edge.sourceNodeId,
-          to: edge.targetNodeId,
-        }))}
-        onModelChange={handleModelChange}
-      />
-    </div>
+    <ReactDiagram
+      skipsDiagramUpdate={false}
+      initDiagram={initDiagram}
+      divClassName="diagram-component"
+      nodeDataArray={props.project.nodes.map((node) => ({
+        key: node.uniqueName,
+        text: node.uniqueName,
+      }))}
+      linkDataArray={props.project.edges.map((edge) => ({
+        key: edge.id,
+        from: edge.sourceNodeId,
+        to: edge.targetNodeId,
+      }))}
+      onModelChange={handleModelChange}
+    />
   );
 }
